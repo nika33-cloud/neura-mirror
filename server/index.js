7adf9a509d13fc551da887a3d4d93405096ac65c
nika33-cloud <goddessnika33@gmail.com>
2026-04-05 06:59:43 +0100

    new

--- server/index.js
+++ server/index.js
@@ -11,25 +11,38 @@ import withdrawRoutes from "./routes/withdraw.routes.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+const dataBase = process.env.MONGODB_URL;
+const client = process.env.CLIENT_URL;
+const admin = process.env.ADMIN_URL;
 
+
+
+const corsconfig = {
+  origin: [admin, client],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsconfig));
 app.use(express.json({ limit: "12mb" }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://neura-mirror.vercel.app",
-    ],
-    credentials: true,
-  })
-);
 
 app.use("/auth", authRoutes);
 app.use("/user", fundRoutes);
 app.use("/transaction", transactRoutes);
 app.use("/withdraw", withdrawRoutes);
+
+mongoose
+  .connect(dataBase)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("API Working");
